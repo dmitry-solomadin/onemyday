e3d5bf7cf1b3537b0273498d62a8c955984e1638
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2012-07-07 14:21:54 +0300

    Initil commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onemyday::Application.config.secret_token = 'd9d3652dab7481adc90521b27df8b2394b75416fe37c077b564ad52d0e30659202449b72d8605aed6da8a351dcdf887f6b6cac97b19ac018eefca5c0dc093827'
