ef8880d5fd80ffdb5d326e376bedff5a104db4d3
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2012-07-14 14:56:50 +0300

    Added facebook integration.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
-puts "initializing twitter with #{ENV['TWITTER_KEY']}, #{ENV['TWITTER_SECRET']}"
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, "3Ot3E3BhpTFV5inPVXOw", "mNtee7A5W87WdMDlIBHCRQURv29ECTsKUlWoArY0A"
+  provider :facebook, "272763286172620", "e347de77c945e9a8ea95e9548e8a9a31"
 end
