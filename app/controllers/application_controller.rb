6e27e7bc0e0f9c1efe3aed772a625b1bd6b053e1
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2013-02-11 23:06:06 +0200

    Added nice photo header.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,6 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = :ru
-    return
-
     if current_user && !current_user.locale.blank?
       I18n.locale = current_user.locale
     else
