c93329fe4b24074b7fed85d7bafc1fbe3be8916c
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2013-04-11 20:00:24 +0300

    Added include user parameter to stories request

--- app/services/story_format.rb
+++ app/services/story_format.rb
@@ -3,6 +3,7 @@ class StoryFormat
   def self.to_json(story, params)
     include = []
     include<<:comments if params[:c]
+    include<<:user if params[:u]
     include<<{:story_photos => {:methods=> :photo_urls}} if params[:p]
     story.to_json(:include => include)
   end
