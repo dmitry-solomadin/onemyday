a9b6b3848a764552f8e3315273432952dc93450b
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2013-05-29 15:29:39 +0300

    Added is_liked_by_user to all stories request.

--- app/services/story_format.rb
+++ app/services/story_format.rb
@@ -5,7 +5,17 @@ class StoryFormat
     include<<:comments if params[:c]
     include<<{:user => {:methods => :avatar_urls}} if params[:u]
     include<<{:story_photos => {:methods => :photo_urls}} if params[:p]
-    story.current_user = User.find(params[:requesting_user_id]) if params[:requesting_user_id]
+
+    # add requesting user to each story model so it can say whether it was liked by him.
+    if params[:requesting_user_id]
+      u = User.find(params[:requesting_user_id])
+      if story.kind_of?(Array)
+        story.each { |s| s.current_user = u }
+      else
+        story.current_user = u
+      end
+    end
+
     story.to_json(:include => include, :methods => :is_liked_by_user)
   end
 
