7bb99e665934a84ac27f23496e0bbaaba0cef6f1
Dmitry Solomadin <dmitry.solomadin@gmail.com>
2013-06-30 00:40:57 +0300

    Added lower_than_id, higher_than_id and limit parameters to the activities request.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,9 @@ class ActivitiesController < ApplicationController
 
   def index
     @activities = User.find(params[:id]).activities.order("created_at DESC")
+    @activities = @activities.where("id > #{params[:higher_than_id]}") if params[:higher_than_id]
+    @activities = @activities.where("id < #{params[:lower_than_id]}") if params[:lower_than_id]
+    @activities = @activities.limit(params[:limit]) if params[:limit]
     @activities.unseen.each do |activity|
       activity.update_attribute(:viewed, true)
     end
